b1aff4295649ed305a7060dddac53903699c4597
Szalontai Jordan <jordanlt1111@gmail.com>
2019-11-22 22:57:59 +0100

    Add styles and fixes

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './PostList.css';
 
 export default function PostList({ username, hashtags = [] }) {
-    const [groups, setGroups] = useState({ groups: {} });
+    const [groups, setGroups] = useState({});
 
     useEffect(() => {
         async function fetchGroups() {
             const hashtagList = hashtags.map(tag => tag.substring(1)).join(',');
-            const response = await fetch(`/api/ig/${username}/1?pageSize=10&hashtags=${hashtagList}&grouped`);
-            const json = await response.json();
+            if (hashtagList.length > 0) {
+                const response = await fetch(`/api/ig/${username}/1?pageSize=10&hashtags=${hashtagList}&grouped`);
+                const json = await response.json();
 
-            setGroups(json.groups);
+                setGroups(json.groups);
+            }
         }
         fetchGroups();
     }, [hashtags]);
@@ -20,18 +22,18 @@ export default function PostList({ username, hashtags = [] }) {
     return (
         <div className="PostList">
             <ul>
-            { Object.keys(groups).filter(key => key !== 'nohashtag').map((key, index) => {
-                return <li key={`group-${index}`}>
-                    <h2>{key}</h2>
-                    <div className="posts">
-                        { groups[key].length === 0 && <div>Soon ➡️</div> }
-                        { groups[key].length > 0 &&
-                            groups[key].map((post, i) => {
-                                return <div key={`post-${index}-${i}`} dangerouslySetInnerHTML={ { __html: post.template }}></div>;
-                            }) }
-                    </div>
-                </li>;
-            })}
+                {Object.keys(groups).filter(key => key !== 'nohashtag').map((key, index) => {
+                    return <li key={`group-${index}`}>
+                        <h2>{key}</h2>
+                        <div className="posts">
+                            {groups[key].length === 0 && <div>Soon ➡️</div>}
+                            {groups[key].length > 0 &&
+                                groups[key].map((post, i) => {
+                                    return <div key={`post-${index}-${i}`} dangerouslySetInnerHTML={{ __html: post.template }}></div>;
+                                })}
+                        </div>
+                    </li>;
+                })}
             </ul>
         </div>
     );
