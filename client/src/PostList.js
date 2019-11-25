d30d9ed67f5986da75a2d0d137095fcda45803b1
Szalontai Jordan <jordanlt1111@gmail.com>
2019-11-25 23:21:07 +0100

    Styles and category fix

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -19,21 +19,34 @@ export default function PostList({ username, hashtags = [] }) {
 
     useEffect(() => window.instgrm && window.instgrm.Embeds.process(), [groups]);
 
+    const activeHashtags = Object.keys(groups)
+        .filter(key => key !== 'nohashtag' && groups[key].length > 0);
+
+    const soonHashtags = Object.keys(groups)
+        .filter(key => key !== 'nohashtag' && groups[key].length === 0)
+        .reduce((a, b) => a.concat(b), []);
+
     return (
         <div className="PostList">
             <ul>
-                {Object.keys(groups).filter(key => key !== 'nohashtag').map((key, index) => {
+                {activeHashtags.map((key, index) => {
                     return <li key={`group-${index}`}>
                         <h2>{key}</h2>
                         <div className="posts">
-                            {groups[key].length === 0 && <div>Soon ➡️</div>}
-                            {groups[key].length > 0 &&
-                                groups[key].map((post, i) => {
-                                    return <div key={`post-${index}-${i}`} dangerouslySetInnerHTML={{ __html: post.template }}></div>;
-                                })}
+                            {groups[key].map((post, i) => {
+                                return <div key={`post-${index}-${i}`} dangerouslySetInnerHTML={{ __html: post.template }}></div>;
+                            })}
                         </div>
                     </li>;
                 })}
+                <li className="soon">
+                    <h2>➡️ Soon</h2>
+                    <div className="soon-tags">
+                        {soonHashtags.map((tag, index) => {
+                            return <div key={`soon-${index}`} className="soon-tag">{tag}</div>;
+                        })}
+                    </div>
+                </li>
             </ul>
         </div>
     );
